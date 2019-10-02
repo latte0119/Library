7305da3a97f84c51ff34e794cfa9eb06bd2ee7d0
Maeda Namiki <latte.malta@gmail.com>
2019-10-02 19:43:38 +0900

    rework

--- src/imperfect/NumberTheoreticTransform.cpp
+++ src/imperfect/NumberTheoreticTransform.cpp
@@ -1,65 +1,57 @@
-const int mods[]={1012924417,998244353,897581057,645922817};
-const int roots[]={5,3,3,3};
+/*
+based on Cooley-Cukey
+O(nlogn)
+*/
+
+template<class Mint>
 struct NumberTheoreticTransform{
-	int mod;
-    int root;
-	NumberTheoreticTransform(int mod,int root):mod(mod),root(root){}
-
-	int mul(int x,int y){
-		return x*y%mod;
-	}
-	int add(int x,int y){
-		return (x+=y)>=mod?x-mod:x;
-	}
-
-	int pow(int x,int y){
-		int res=1;
-		while(y){
-			if(y&1)res=mul(res,x);
-			x=mul(x,x);
-			y>>=1;
-		}
-		return res;
-	}
-
-	int inv(int x){
-		return pow(x,mod-2);
-	}
-
-	void ntt(vector<int>&f,bool rev=false){
+    Mint root;
+	NumberTheoreticTransform(Mint root):root(root){}
+	void ntt(vector<Mint>&f){
 		int n=f.size();
         for(int i=0,j=1;j<n-1;j++){
-            for(int k=n>>1;k>(i^=k);k/=2);
+            for(int k=n>>1;k>(i^=k);k>>=1);
             if(i>j)swap(f[i],f[j]);
         }
 
-     	for(int m=2;m<=n;m*=2){
-            int wr=pow(root,(mod-1)/m);
-            if(rev)wr=inv(wr);
-            for(int i=0;i<n;i+=m){
-                int w=1;
-                for(int j=0;j<m/2;j++){
-                    int f0=f[i+j],f1=mul(w,f[i+j+m/2]);
-                    f[i+j]=add(f0,f1);
-                    f[i+j+m/2]=add(f0,mod-f1);
-                    w=mul(w,wr);
+        int s=__lg(n);
+
+        for(int m=1;m<=s;m++){
+            Mint wr=root.pow(Mint(-1).a/(1<<m));
+            for(int i=0;i<n;i+=1<<m){
+                Mint w=1;
+                for(int j=0;j<1<<m-1;j++){
+                    Mint f0=f[i+j],f1=w*f[i+j+(1<<m-1)];
+                    f[i+j]=f0+f1;
+                    f[i+j+(1<<m-1)]=f0-f1;
+                    w*=wr;
                 }
             }
         }
-        if(rev){
-        	int v=inv(n);
-        	for(int i=0;i<n;i++)f[i]=mul(f[i],v);
-		}
 	}
-	vector<int>multiply(vector<int>A,vector<int>B){
-        int n=1;
-        while(n<A.size()+B.size()-1)n<<=1;
+
+    void intt(vector<Mint>&f){
+        reverse(f.begin()+1,f.end());
+        ntt(f);
+        Mint in=Mint(f.size()).inv();
+        for(int i=0;i<f.size();i++)f[i]*=in;
+    }
+
+	vector<Mint>convolute(vector<Mint>A,vector<Mint>B){
+        int m=A.size()+B.size()-1;
+        int n=1<<__lg(m-1)+1;
         A.resize(n);B.resize(n);
         ntt(A);
         ntt(B);
-        for(int i=0;i<n;i++)A[i]=mul(A[i],B[i]);
-        ntt(A,true);
-        A.resize(A.size()+B.size()-1);
+        for(int i=0;i<n;i++)A[i]*=B[i];
+        intt(A);
+        A.resize(m);
         return A;
     }
 };
+
+
+/*
+verified:
+https://atc001.contest.atcoder.jp/submissions/7823077
+*/
