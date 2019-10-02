a6e4259c23c0083b2157e58ffc7cd8267b159630
Maeda Namiki <latte.malta@gmail.com>
2019-10-02 23:46:39 +0900

    NTTをstatic化

--- src/math/NumberTheoreticTransform.cpp
+++ src/math/NumberTheoreticTransform.cpp
@@ -3,21 +3,19 @@ based on Cooley-Tukey
 O(nlogn)
 */
 
-template<class Mint>
+template<class Mint,int32_t root>
 struct NumberTheoreticTransform{
-    Mint root;
-	NumberTheoreticTransform(Mint root):root(root){}
-	void ntt(vector<Mint>&f){
+	static void ntt(vector<Mint>&f){
 		int n=f.size();
+        int s=__lg(n);
+
         for(int i=0,j=1;j<n-1;j++){
             for(int k=n>>1;k>(i^=k);k>>=1);
             if(i>j)swap(f[i],f[j]);
         }
 
-        int s=__lg(n);
-
         for(int m=1;m<=s;m++){
-            Mint wr=root.pow(Mint(-1).a/(1<<m));
+            Mint wr=Mint(root).pow(Mint(-1).a>>m);
             for(int i=0;i<n;i+=1<<m){
                 Mint w=1;
                 for(int j=0;j<1<<m-1;j++){
@@ -30,28 +28,31 @@ struct NumberTheoreticTransform{
         }
 	}
 
-    void intt(vector<Mint>&f){
+    static void intt(vector<Mint>&f){
         reverse(f.begin()+1,f.end());
         ntt(f);
         Mint in=Mint(f.size()).inv();
         for(int i=0;i<f.size();i++)f[i]*=in;
     }
 
-	vector<Mint>convolute(vector<Mint>A,vector<Mint>B){
-        int m=A.size()+B.size()-1;
-        int n=1<<__lg(m-1)+1;
-        A.resize(n);B.resize(n);
-        ntt(A);
-        ntt(B);
-        for(int i=0;i<n;i++)A[i]*=B[i];
-        intt(A);
-        A.resize(m);
-        return A;
+	static vector<Mint>convolute(const vector<Mint>&A,const vector<Mint>&B){
+        int n=1<<__lg(A.size()+B.size()-2)+1;
+        
+        vector<Mint>a=A,b=B;
+        a.resize(n);b.resize(n);
+        ntt(a);
+        ntt(b);
+        for(int i=0;i<n;i++)a[i]*=b[i];
+        intt(a);
+        a.resize(A.size()+B.size()-1);
+        return a;
     }
 };
+//using NTT=NumberTheoreticTransform<mint,3>;
+
 
 
 /*
 verified:
-https://atc001.contest.atcoder.jp/submissions/7823077
+https://atc001.contest.atcoder.jp/submissions/7826555
 */
