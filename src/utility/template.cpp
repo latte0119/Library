ddc54c3e7fe1dc999baddda46d96e7cf6d211b04
Maeda Namiki <latte.malta@gmail.com>
2019-10-30 22:10:16 +0900

    add eb

--- src/utility/template.cpp
+++ src/utility/template.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 #define rep(i,n) for(int i=0;i<(n);i++)
 #define pb push_back
+#define eb emplace_back
 #define all(v) (v).begin(),(v).end()
 #define fi first
 #define se second
@@ -33,6 +34,5 @@ ostream& operator<<(ostream& ost,const vector<T>&v){
 }
 
 signed main(){
-
-    return 0;
+	
 }
