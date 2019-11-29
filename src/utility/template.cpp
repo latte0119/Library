f55c1a8f487f0016cdbfa08333ae3b7fe864f044
malta <latte.malta@gmail.com>
2019-11-29 23:01:55 +0900

    reps

--- src/utility/template.cpp
+++ src/utility/template.cpp
@@ -4,6 +4,7 @@ using namespace std;
 #define int long long
 
 #define rep(i,n) for(int i=0;i<(n);i++)
+#define reps(i,a,b) for(int i=(a);i<(b);i++)
 #define pb push_back
 #define eb emplace_back
 #define all(v) (v).begin(),(v).end()
@@ -19,21 +20,21 @@ template<typename A,typename B>inline void chmax(A &a,B b){if(a<b)a=b;}
 
 template<class A,class B>
 ostream& operator<<(ostream& ost,const pair<A,B>&p){
-	ost<<"{"<<p.first<<","<<p.second<<"}";
-	return ost;
+    ost<<"{"<<p.first<<","<<p.second<<"}";
+    return ost;
 }
 
 template<class T>
 ostream& operator<<(ostream& ost,const vector<T>&v){
-	ost<<"{";
-	for(int i=0;i<v.size();i++){
-		if(i)ost<<",";
-		ost<<v[i];
-	}
-	ost<<"}";
-	return ost;
+    ost<<"{";
+    for(int i=0;i<v.size();i++){
+        if(i)ost<<",";
+        ost<<v[i];
+    }
+    ost<<"}";
+    return ost;
 }
 
 signed main(){
-	return 0;
+    return 0;
 }
