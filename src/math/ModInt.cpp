87a48960d4f77a1034679f447c76bf6276cf8812
malta <latte.malta@gmail.com>
2019-11-29 23:57:54 +0900

    mtable

--- src/math/ModInt.cpp
+++ src/math/ModInt.cpp
@@ -1,8 +1,3 @@
-/*
-GF(p)
-inverse:O(log p) based on Fermat's little theorem (a^(p-1)=1 mod p)
-*/
-
 template<uint32_t mod>
 struct ModInt{
 	uint32_t a;
@@ -56,27 +51,3 @@ ostream& operator<<(ostream& out,const ModInt<mod>& a){
 }
 using mint=ModInt<1000000007>;
 //using mint=ModInt<998244353>;
-
-
-template<class Mint,int32_t lg>
-struct ModIntTable{
-	int N;
-	vector<Mint>facts,finvs,invs;
-	ModIntTable():N(1<<lg),facts(N),finvs(N),invs(N){
-		const uint32_t mod=Mint(-1).a+1;
-		invs[1]=1;
-		for(int i=2;i<N;i++)invs[i]=invs[mod%i]*(mod-mod/i);
-
-		facts[0]=1;
-		finvs[0]=1;
-		for(int i=1;i<N;i++){
-			facts[i]=facts[i-1]*i;
-			finvs[i]=finvs[i-1]*invs[i];
-		}
-	}
-	inline Mint fact(int n)const{return facts[n];}
-	inline Mint finv(int n)const{return finvs[n];}
-	inline Mint inv(int n)const{return invs[n];}
-	inline Mint binom(int n,int k)const{return facts[n]*finvs[k]*finvs[n-k];}
-};
-ModIntTable<mint,19>mtable;
---
+++ src/math/ModIntTable.cpp
@@ -0,0 +1,23 @@
+template<class Mint,int32_t lg>
+struct ModIntTable{
+	int N;
+	vector<Mint>facts,finvs,invs;
+	ModIntTable():N(1<<lg),facts(N),finvs(N),invs(N){
+		const uint32_t mod=Mint(-1).a+1;
+		invs[1]=1;
+		for(int i=2;i<N;i++)invs[i]=invs[mod%i]*(mod-mod/i);
+
+		facts[0]=1;
+		finvs[0]=1;
+		for(int i=1;i<N;i++){
+			facts[i]=facts[i-1]*i;
+			finvs[i]=finvs[i-1]*invs[i];
+		}
+	}
+	inline Mint fact(int n)const{return facts[n];}
+	inline Mint finv(int n)const{return finvs[n];}
+	inline Mint inv(int n)const{return invs[n];}
+	inline Mint binom(int n,int k)const{return facts[n]*finvs[k]*finvs[n-k];}
+    inline Mint perm(int n,int k)const{return facts[n]*finvs[n-k];} 
+};
+ModIntTable<mint,19>mtable;
