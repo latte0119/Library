6a0875b8c1b14c5915944ab35f3da87e95db56b9
malta <latte.malta@gmail.com>
2019-12-03 01:38:41 +0900

    sparse table update

--- src/data-structure/SparseTable.cpp
+++ src/data-structure/SparseTable.cpp
@@ -1,21 +1,21 @@
 template<class T>
-    struct sparse_table{
-        vector<vector<T>>st;
-        void init(const vector<T>&vec){
-            int b;
-            for(b=0;(1<<b)<=vec.size();b++);
-            st.assign(b,vector<T>(1<<b));
-            for(int i=0;i<vec.size();i++)st[0][i]=vec[i];
+struct SparseTable{
+    vector<vector<T>>st;
+    void init(const vector<T>&vec){
+        int32_t b;
+        for(b=0;(1<<b)<=vec.size();b++);
+        st.assign(b,vector<T>(1<<b));
+        for(int32_t i=0;i<vec.size();i++)st[0][i]=vec[i];
 
-            for(int i=1;i<b;i++){
-                for(int j=0;j+(1<<i)<=(1<<b);j++){
-                    st[i][j]=min(st[i-1][j],st[i-1][j+(1<<(i-1))]);
-                }
+        for(int32_t i=1;i<b;i++){
+            for(int32_t j=0;j+(1<<i)<=(1<<b);j++){
+                st[i][j]=min(st[i-1][j],st[i-1][j+(1<<(i-1))]);
             }
         }
-        T get_min(int l,int r){
-            assert(l<r);
-            int b=32-__builtin_clz(r-l)-1;
-            return min(st[b][l],st[b][r-(1<<b)]);
-        }
-    };
+    }
+    T getMin(int32_t l,int32_t r){
+        assert(l<r);
+        int32_t b=32-__builtin_clz(r-l)-1;
+        return min(st[b][l],st[b][r-(1<<b)]);
+    }
+};
