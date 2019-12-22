8c7cb844eb10e6caf9e3596308a629080b303640
malta <latte.malta@gmail.com>
2019-12-22 23:22:13 +0900

    bug fix

--- src/graph/Cactus.cpp
+++ src/graph/Cactus.cpp
@@ -1,5 +1,5 @@
 /*
-c.size()==2 -> edge
+c.size()==2 -> bridge
 c.size()>2 -> cycle
 */
 struct Cactus{
@@ -7,7 +7,7 @@ struct Cactus{
 
     
     vector<vector<int>>G;
-    vector<int>vis,par,dep;
+    vector<int>vis,par,dep,mark;
     void dfs(int v){
         vis[v]=1;
         for(auto u:G[v]){
@@ -16,10 +16,11 @@ struct Cactus{
                 dep[u]=dep[v]+1;
                 dfs(u);
             }
-            else if(dep[u]<dep[v]){
+            else if(dep[u]<dep[v]-1){
                 int w=v;
                 vector<int>c{w};
                 while(true){
+					mark[w]=true;
                     w=par[w];
                     c.push_back(w);
                     if(w==u)break;
@@ -28,10 +29,19 @@ struct Cactus{
             }
         }
     }
-    Cactus(const vector<vector<int>>&G):G(G){
+
+    vector<vector<int>>decompose(const vector<vector<int>>&G_){
+		G=G_;
         vis.resize(G.size());
         par.resize(G.size());
         dep.resize(G.size());
+		mark.resize(G.size());
         dfs(0);
+		for(int i=1;i<G.size();i++)if(!mark[i]){
+			vector<int>tmp={i,par[i]};
+			cs.push_back(tmp);
+		}
+		return cs;
     }
+	Cactus(){}
 };
