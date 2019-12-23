9d2a45964eb055a8c02c318bd0aa4822fb4aa883
malta <latte.malta@gmail.com>
2019-12-23 18:14:31 +0900

    ahocora

--- src/string/AhoCorasick.cpp
+++ src/string/AhoCorasick.cpp
@@ -2,20 +2,22 @@ template<class C>
 struct AhoCorasick{
 	struct Node{
 		map<C,int>nex;
-		int failink;
+		int link;
 		int len;
-		Node():failink(0),len(0){}
+		int par;
+		Node():link(0),len(0),par(0){}
 	};
 	vector<Node>nds;
 	AhoCorasick(){
 		nds.emplace_back();
-		nds[0].failink=-1;
+		nds[0].link=-1;
+		nds[0].par=-1;
 	}
 
 	int process(int k,const C &c){
+		while(k!=-1&&nds[k].nex.find(c)==nds[k].nex.end())k=nds[k].link;
         if(k==-1)return 0;
-        if(nds[k].nex.find(c)!=nds[k].nex.end())return nds[k].nex[c];
-		return nds[k].nex[c]=process(nds[k].failink,c);
+        return nds[k].nex[c];
 	}
 
     inline int addChar(int k,const C &c){
@@ -23,6 +25,7 @@ struct AhoCorasick{
             nds[k].nex[c]=nds.size();
             nds.emplace_back();
             nds.back().len=nds[k].len+1;
+			nds.back().par=k;
         }
         return nds[k].nex[c];
     }
@@ -43,9 +46,12 @@ struct AhoCorasick{
 			for(auto &p:nds[k].nex){
 				C c=p.first;
 				int nx=p.second;
-				nds[nx].failink=process(nds[k].failink,c);
+				nds[nx].link=process(nds[k].link,c);
 				que.push(nx);
 			}
 		}
 	}
+
+	Node& operator[](int k){return nds[k];}
+	inline int size(){return nds.size();}
 };
